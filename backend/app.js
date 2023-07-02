42563b4bb4abd059c1092451677dd724297c22cc
RohanM2000 <roh20rocks@gmail.com>
2023-07-02 00:20:17 -0700

    Finish user registration

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const { isProduction } = require('./config/keys');
 const csurf = require('csurf');
 const debug = require('debug');
 
+require('./models/User');
+
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
 const csrfRouter = require('./routes/api/csrf');
