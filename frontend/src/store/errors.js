fac640468eb36fc5c0a272e8edd7d639186423a8
RohanM2000 <roh20rocks@gmail.com>
2023-07-02 04:18:16 -0700

    Frontend tweets

--- frontend/src/store/errors.js
+++ frontend/src/store/errors.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import { sessionErrorsReducer } from './session';
-
+import { tweetErrorsReducer } from "./tweets";
 export default combineReducers({
-    session: sessionErrorsReducer
+    session: sessionErrorsReducer,
+    tweets: tweetErrorsReducer
 });
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import session from './sessoin';
-
+import session from './session';
+import tweets from './tweets';
 const rootReducer = combineReducers({
-  session
+  session,
+  tweets
 });
 
 let enhancer;
