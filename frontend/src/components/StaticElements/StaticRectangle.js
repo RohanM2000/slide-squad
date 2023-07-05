fb4c8e7d4e95be021dae0b6cf57b44645d03bc3b
RohanM2000 <roh20rocks@gmail.com>
2023-07-05 14:44:48 -0700

    Add colors to static elements

--- frontend/src/components/StaticElements/StaticRectangle.js
+++ frontend/src/components/StaticElements/StaticRectangle.js
@@ -1,11 +1,11 @@
-export default function StaticRectangle ({startLeft, id, startTop, startWidth, startHeight, setPresentationState, windowHeight, windowWidth}) {
+export default function StaticRectangle ({startLeft, id, startTop, startWidth, startHeight, setPresentationState, windowHeight, windowWidth, bg}) {
     return (
         <div
             style={{position: "absolute", 
                     display: "flex",
                     "justify-content": "flex-end",
                     "align-items": "flex-end",
-                    "background-color": "gray",
+                    backgroundColor: bg,
                     top: (startTop*windowHeight) + "px", 
                     left: (startLeft*windowWidth) + "px", 
                     width: (startWidth*windowWidth) + "px",
