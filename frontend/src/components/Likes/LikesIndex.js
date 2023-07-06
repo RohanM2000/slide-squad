bfba37f2cd1f32997d37463ce0e39f0d4af2edb6
RohanM2000 <roh20rocks@gmail.com>
2023-07-05 19:57:23 -0700

    UGLY LIKE DELETE BUTTON FIX

--- frontend/src/components/Likes/LikesIndex.js
+++ frontend/src/components/Likes/LikesIndex.js
@@ -5,28 +5,34 @@ import { fetchPresentations } from '../../store/presentations';
 import { fetchUserLikes, fetchLikes } from '../../store/likes';
 import LikesIndexItem from './LikesIndexItem';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
-
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 const LikesIndex = () => {
     const { userId } = useParams();
     const dispatch = useDispatch();
-    const likes = useSelector(state => Object.values(state.likes));
-    
+    const [newLikes, setNewLikes] = useState([])
+    const likes = useSelector(state => {
+      // console.log(state.likes);
+      if (Object.values(state.likes).length !== newLikes.length) {
+        setNewLikes(Object.values(state.likes));
+      }
+      return state.likes
+    });
     useEffect(() => {
       dispatch(fetchUserLikes(userId));
     }, [dispatch, userId])
 
-    console.log(likes);
+    // console.log("likes", likes);
   
   
-  
+    console.log("RERENDERING RIGHT NOW")
     return (
       <div>
         <h2>Presentations Liked by User</h2>
         <div className='likes-container'>
-            {likes.map(like => (
+            {newLikes.map(like => (
                 <>
-                <LikesIndexItem key={like._id} like={like}/>
+                  <LikesIndexItem key={like._id} like={like} />
                 </>
             ))}
         </div>
--- frontend/src/components/Likes/LikesIndexItem.js
+++ frontend/src/components/Likes/LikesIndexItem.js
@@ -1,12 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './Likes.css';
 import { deleteLike } from '../../store/likes';
 import { fetchPresentation } from '../../store/presentations';
 
 const LikesIndexItem = ({ like } ) => {
-    const presentation = useSelector(state => state.presentations[like.likeId] )
+    const presentation = useSelector(state => state.presentations[like.likeId] );
     const dispatch = useDispatch();
+    const [show, setShow] = useState(true);
     useEffect(() => {
         dispatch(fetchPresentation(like.likeId))
     }, [dispatch, like.likeId])
@@ -14,14 +15,16 @@ const LikesIndexItem = ({ like } ) => {
     if (presentation === undefined) {
         return null 
     }
+    // console.log("like", like);
 
-    console.log(like);
-
-    return (
+    return like && show && (
         <>
         <div className='like-container'>
             <h3>{presentation.title}</h3>
-            <button onClick={() => dispatch(deleteLike(like._id))}>Delete</button>
+            <button onClick={() => {
+                dispatch(deleteLike(like._id));
+                setShow(false);
+            }}>Delete</button>
         </div>
         </>
     )
