cf650b4b69c6ca49156285e11425d4e34da9bbfd
RohanM2000 <roh20rocks@gmail.com>
2023-07-05 17:10:21 -0700

    More small backend fixes

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -30,72 +30,18 @@ router.post('/', requireUser, validateCommentInput, async (req, res, next) => {
 
     const comment = await newComment.save();
 
-    // Add the comment to the presentation's comments array
-    // presentation.comments.push(comment._id);
-    // await presentation.save();
 
     return res.json(comment);
-  } catch (err) {
-    next(err);
-    // return res.json("erroring out here");
-  }
-});
-
-// Reply to a comment
-router.post('/:presentationId/comments/:commentId/reply', requireUser, async (req, res, next) => {
-  try {
-    const presentationId = req.params.presentationId;
-    const commentId = req.params.commentId;
-    const { content } = req.body;
-
-    const presentation = await Presentation.findById(presentationId);
-    if (!presentation) {
-      const error = new Error('Presentation not found');
-      error.statusCode = 404;
-      error.errors = { message: 'No presentation found with that id' };
-      return next(error);
-    }
-
-    const parentComment = await Comment.findById(commentId);
-    if (!parentComment) {
-      const error = new Error('Comment not found');
-      error.statusCode = 404;
-      error.errors = { message: 'No comment found with that id' };
-      return next(error);
-    }
-
-    const newReply = new Comment({
-      user: req.user._id,
-      content,
-      parent_id: commentId
-    });
-
-    const reply = await newReply.save();
-
-    // Add the reply to the parent comment's replies array
-    parentComment.replies.push(reply._id);
-    await parentComment.save();
-
-    return res.json(reply);
   } catch (err) {
     next(err);
   }
 });
 
 // Edit a comment
-router.patch('/:presentationId/comments/:commentId', requireUser, async (req, res, next) => {
+router.patch('/:commentId', requireUser, async (req, res, next) => {
   try {
-    const presentationId = req.params.presentationId;
     const commentId = req.params.commentId;
     const { content } = req.body;
-
-    const presentation = await Presentation.findById(presentationId);
-    if (!presentation) {
-      const error = new Error('Presentation not found');
-      error.statusCode = 404;
-      error.errors = { message: 'No presentation found with that id' };
-      return next(error);
-    }
 
     const comment = await Comment.findById(commentId);
     if (!comment) {
@@ -106,7 +52,7 @@ router.patch('/:presentationId/comments/:commentId', requireUser, async (req, re
     }
 
     // Check if the user is the owner of the comment or has sufficient privileges
-    if (comment.user.toString() !== req.user._id.toString()) {
+    if (comment.user._id.toString() !== req.user._id.toString()) {
       const error = new Error('Unauthorized');
       error.statusCode = 401;
       error.errors = { message: 'You are not authorized to edit this comment' };
@@ -123,18 +69,11 @@ router.patch('/:presentationId/comments/:commentId', requireUser, async (req, re
 });
 
 // Delete a comment
-router.delete('/:presentationId/comments/:commentId', requireUser, async (req, res, next) => {
+router.delete('/:commentId', requireUser, async (req, res, next) => {
   try {
-    const presentationId = req.params.presentationId;
     const commentId = req.params.commentId;
 
     const presentation = await Presentation.findById(presentationId);
-    if (!presentation) {
-      const error = new Error('Presentation not found');
-      error.statusCode = 404;
-      error.errors = { message: 'No presentation found with that id' };
-      return next(error);
-    }
 
     const comment = await Comment.findById(commentId);
     if (!comment) {
@@ -145,23 +84,11 @@ router.delete('/:presentationId/comments/:commentId', requireUser, async (req, r
     }
 
     // Check if the user is the owner of the comment or has sufficient privileges
-    if (comment.user.toString() !== req.user._id.toString()) {
+    if (comment.user._id.toString() !== req.user._id.toString()) {
       const error = new Error('Unauthorized');
       error.statusCode = 401;
       error.errors = { message: 'You are not authorized to delete this comment' };
       return next(error);
-    }
-
-    // Remove the comment from the parent's comments array
-    if (comment.parent_id) {
-      const parentComment = await Comment.findById(comment.parent_id);
-      if (parentComment) {
-        parentComment.replies.pull(comment._id);
-        await parentComment.save();
-      } else {
-        presentation.comments.pull(comment._id);
-        await presentation.save();
-      }
     }
 
     // Delete the comment
