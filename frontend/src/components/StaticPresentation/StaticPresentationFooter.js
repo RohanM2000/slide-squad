80ffd272a1dccc9d7fa5969339b907aeee78f8df
vincentpham123 <vincentpham32@gmail.com>
2023-07-06 11:24:22 -0700

    add comments toggler footer

---
+++ frontend/src/components/StaticPresentation/StaticPresentationFooter.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min"
+import CommentsIndex from "../Comments/CommentIndex";
+
+const PresentationFooter =()=>{
+    const {presentationId} = useParams();
+    const [showComments,setShowComments] = useState(false);
+
+    const handleToggle=()=>{
+        setShowComments(!showComments);
+    }
+    return (
+        <div className='footer-container'>
+            { !showComments && <div onClick={handleToggle} className='footer-toggle'>
+                <i className="fa-solid fa-arrow-down"></i>
+            </div>}
+            { showComments && <div onClick={handleToggle} className='footer-toggle'>
+                <i className="fa-solid fa-arrow-up"></i>
+            </div>}
+            {showComments && <CommentsIndex />}
+        </div>
+    )
+}
+
+export default PresentationFooter;
